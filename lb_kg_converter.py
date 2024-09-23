d589fad8d286e4c9e094c85f7a82a7eb997ff827
Ember-132 <emma.raymond@btinternet.com>
2024-09-23 11:02:46 +0100

    Update lb_kg_converter.py

--- lb_kg_converter.py
+++ lb_kg_converter.py
@@ -2,9 +2,6 @@
 weight = float(input("Please enter weight: "))
 measurement = input("What measurement have you used? Type 'L' for pounds or 'K' for kilograms: ")
 
-
-
-
 if measurement == "L" or measurement == "l":
     new_weight = weight * 0.45359237
     print(f"You weigh {new_weight} kg")
